dd4f27488ced6fea23f19b46d1647e411edbc157
vikctar <vikc.steve@gmail.com>
2016-05-26 23:05:44 +0300

    Create subpackages

--- app/src/main/java/com/vikcandroid/retrofitdemo/activity/MainActivity.java
+++ app/src/main/java/com/vikcandroid/retrofitdemo/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.vikcandroid.retrofitdemo;
+package com.vikcandroid.retrofitdemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vikcandroid.retrofitdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
